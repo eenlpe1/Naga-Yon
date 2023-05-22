b1ff1a806b4fa36695e6e10ba2e12bb78e181e34
Emmanuel <emmanuelenalpe@gmail.com>
2023-05-22 23:06:49 +0800

    Update park tab

--- lib/tab/parktab.dart
+++ lib/tab/parktab.dart
@@ -102,17 +102,26 @@ class ParkDetailsScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(destination),
+        title: Text(
+          destination,
+          style:
+              const TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
+        ),
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        iconTheme: const IconThemeData(
+          color: Colors.black,
+        ),
       ),
       body: Padding(
-        padding: const EdgeInsets.all(16.0),
+        padding: const EdgeInsets.fromLTRB(16.0, 0.0, 16.0, 0.0),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Image.asset(
               imageUrl,
               width: double.infinity,
-              height: 250.0,
+              height: 200.0,
               fit: BoxFit.cover,
             ),
             const SizedBox(height: 20.0),
@@ -124,8 +133,8 @@ class ParkDetailsScreen extends StatelessWidget {
               ),
             ),
             const SizedBox(height: 5.0),
-            const Row(
-              children: [
+            Row(
+              children: const [
                 Icon(Icons.star, color: Colors.amber),
                 Icon(Icons.star, color: Colors.amber),
                 Icon(Icons.star, color: Colors.amber),
@@ -134,13 +143,15 @@ class ParkDetailsScreen extends StatelessWidget {
               ],
             ),
             const SizedBox(height: 20.0),
-            Expanded(
-              child: Text(
-                description,
-                textAlign: TextAlign.justify,
+            Flexible(
+              child: SingleChildScrollView(
+                child: Text(
+                  description,
+                  textAlign: TextAlign.justify,
+                ),
               ),
             ),
-            const SizedBox(height: 5.0),
+            const SizedBox(height: 50),
             Row(
               children: [
                 const Icon(Icons.access_time),
@@ -164,13 +175,15 @@ class ParkDetailsScreen extends StatelessWidget {
                 Text(location),
               ],
             ),
-            const SizedBox(height: 16.0),
-             GestureDetector(
+            const SizedBox(height: 10.0),
+            GestureDetector(
               onTap: () {
                 Navigator.pushReplacement(
                   context,
                   MaterialPageRoute(
-                    builder: (context) => BookScreen(destination: destination),
+                    builder: (context) => const BookScreen(
+                      destination: '',
+                    ),
                   ),
                 );
               },
@@ -204,62 +217,80 @@ class ParkTab extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListView(scrollDirection: Axis.vertical, 
-      children: const [
-        ParkCard(
-          imageUrl: 'assets/travelSpots/CalauitSafariPark.jpg',
-          destination: 'Calauit Safari Park',
-          description:
-            'The Calauit Safari Park is a 3,700-hectare game reserve and wildlife sanctuary. This features African giraffes and zebras carried by a supposed translocation habitat trial involving Kenyan President Jomo Kenyatta and Philippine President Ferdinand Marcos.',
-          openHour: '9:00 AM',
-          closingTime: '6:00 PM',
-          contactNumber: '(02) 524-9174',
-          location: 'Busuanga, Palawan, Philippines',
+    return ListView(
+      scrollDirection: Axis.vertical,
+      children: [
+        Container(
+          margin: const EdgeInsets.only(
+              bottom: 16.0), // Add bottom margin for spacing
+          child: const ParkCard(
+            imageUrl: 'assets/travelSpots/CalauitSafariPark.jpg',
+            destination: 'Calauit Safari Park',
+            description:
+                'The Calauit Safari Park is a 3,700-hectare game reserve and wildlife sanctuary. This features African giraffes and zebras carried by a supposed translocation habitat trial involving Kenyan President Jomo Kenyatta and Philippine President Ferdinand Marcos.',
+            openHour: '9:00 AM',
+            closingTime: '6:00 PM',
+            contactNumber: '(02) 524-9174',
+            location: 'Busuanga, Palawan, Philippines',
+          ),
         ),
-        ParkCard(
-          imageUrl: 'assets/travelSpots/Wildlife.jpg',
-          destination: 'Albay Park and Wildlife',
-          description:
-              'Albay Park and Wildlife (APW) serves as the repository and rehabilitation facility for confiscated, donated, retrieved, turned-over or abandoned wildlife species.',
-          openHour: '9:00 AM',
-          closingTime: '6:00 PM',
-          contactNumber: '(02) 929-8418 ',
-          location: 'Albay',
+        Container(
+          margin: const EdgeInsets.only(
+              bottom: 16.0), // Add bottom margin for spacing
+          child: const ParkCard(
+            imageUrl: 'assets/travelSpots/Wildlife.jpg',
+            destination: 'Albay Park and Wildlife',
+            description:
+                'Albay Park and Wildlife (APW) serves as the repository and rehabilitation facility for confiscated, donated, retrieved, turned-over or abandoned wildlife species.',
+            openHour: '9:00 AM',
+            closingTime: '6:00 PM',
+            contactNumber: '(02) 929-8418 ',
+            location: 'Albay',
+          ),
         ),
-        ParkCard(
-          imageUrl: 'assets/travelSpots/FarmPlate.jpg',
-          destination: 'Farm Plate',
-          description:
-              'FarmPlate is Bicols newest best spot to view the Mayon Volcano.',
-          location: 'Daraga, Albay',
-          contactNumber: '(02) 806-8651 ',
-          openHour: '10:00 AM',
-          closingTime: '7:00 PM',
+        Container(
+          margin: const EdgeInsets.only(
+              bottom: 16.0), // Add bottom margin for spacing
+          child: const ParkCard(
+            imageUrl: 'assets/travelSpots/FarmPlate.jpg',
+            destination: 'Farm Plate',
+            description:
+                'FarmPlate is Bicols newest best spot to view the Mayon Volcano.',
+            location: 'Daraga, Albay',
+            contactNumber: '(02) 806-8651 ',
+            openHour: '10:00 AM',
+            closingTime: '7:00 PM',
+          ),
         ),
-        ParkCard(
-          imageUrl: 'assets/travelSpots/SamboFarm.jpg',
-          destination: 'Cagayan de Oro',
-          description:
-              'Sambo Farm is definitely the perfect hangout place to chill and relax while enjoying the sunset and the beauty of nature. You can bring your friends and family for a picnic date or just roam around the area and feel the summer breeze.',
-          location: 'Magarao, Camarines Sur',
-          contactNumber: '(02) 929-8469 / (02) 929-8418',
-          openHour: '10:00 AM',
-          closingTime: '7:00 PM',
+        Container(
+          margin: const EdgeInsets.only(
+              bottom: 16.0), // Add bottom margin for spacing
+          child: const ParkCard(
+            imageUrl: 'assets/travelSpots/SamboFarm.jpg',
+            destination: 'Cagayan de Oro',
+            description:
+                'Sambo Farm is definitely the perfect hangout place to chill and relax while enjoying the sunset and the beauty of nature. You can bring your friends and family for a picnic date or just roam around the area and feel the summer breeze.',
+            location: 'Magarao, Camarines Sur',
+            contactNumber: '(02) 929-8469 / (02) 929-8418',
+            openHour: '10:00 AM',
+            closingTime: '7:00 PM',
+          ),
         ),
-        ParkCard(
-          imageUrl: 'assets/travelSpots/MayonSkyline.jpg',
-          destination: 'Mayon Skyline',
-          description:
-              'One of the top tourist destination of Albay ONE is the Mayon Skyline (formerly known as Mayon Rest House), it is a facility seated 1000 meters above sea level right at the slope of the Philippines active cone shape volcano, Mayon Volcano.',
-          location: 'Tabaco',
-          contactNumber: '+63(2)2417960 ',
-          openHour: '10:00 AM',
-          closingTime: '7:00 PM',
+        Container(
+          margin: const EdgeInsets.only(
+              bottom: 16.0), // Add bottom margin for spacing
+          child: const ParkCard(
+            imageUrl: 'assets/travelSpots/MayonSkyline.jpg',
+            destination: 'Mayon Skyline',
+            description:
+                'One of the top tourist destinations of Albay ONE is the Mayon Skyline (formerly known as Mayon Rest House), it is a facility seated 1000 meters above sea level right at the slope of the Philippines active cone-shaped volcano, Mayon Volcano.',
+            location: 'Tabaco',
+            contactNumber: '+63(2)2417960 ',
+            openHour: '10:00 AM',
+            closingTime: '7:00 PM',
+          ),
         ),
-      ]
-    ); 
+      ],
+    );
   }
-
 }
-
-
